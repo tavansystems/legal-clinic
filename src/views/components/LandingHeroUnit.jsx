ec97b02d9b72b12ee4f28520176dae17c8f1908f
abdu997 <aamou094@uottawa.ca>
2020-11-25 13:32:30 -0500

    Added welcome message to landing page

--- src/views/components/LandingHeroUnit.jsx
+++ src/views/components/LandingHeroUnit.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grow from '@material-ui/core/Grow';
 
+import LangContext from "../../utils/LangContext"
+
 const useStyles = makeStyles((theme) => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 9),
   },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
+  welcome: {
+    marginBottom: "0px"
+  }
 }));
 
 export default function LandingHeroUnit({ title, breadCrumbs }) {
   const classes = useStyles();
   const [checked, setChecked] = useState(false)
+  const { literals } = useContext(LangContext)
 
   useEffect(() => {
     setChecked(true)
@@ -28,20 +31,15 @@ export default function LandingHeroUnit({ title, breadCrumbs }) {
       <div className={classes.heroContent}>
         <Container maxWidth="sm">
           <Typography
+            className={classes.welcome}
             component="h1"
             variant="h2"
             align="center"
             color="textPrimary"
             gutterBottom
           >
-            {title}
+            {literals.welcome}
           </Typography>
-          {/* <Typography variant="h5" align="center" color="textSecondary" paragraph>
-          DESC. PLACEHOLDER
-        </Typography> */}
-          <div className={classes.heroButtons}>
-
-          </div>
         </Container>
       </div>
     </Grow>
